d1b87b1db4746772857177abeccd3742f39f6576
이진우 <jinus7542@gmail.com>
2020-12-24 16:58:43 +0900

    refactoring.

--- unity-example/Assets/Scripts/Client/Global.cs
+++ unity-example/Assets/Scripts/Client/Global.cs
@@ -1,6 +1,6 @@
 ﻿public class Global
 {
-    public const string restUrl = "https://rcm4u19fbj.execute-api.us-east-1.amazonaws.com/stage/public";
+    public const string restUrl = "https://y47frckbx3.execute-api.us-east-1.amazonaws.com/stage/public";
     public const string wsUrl = "wss://dkhfecq7hg.execute-api.us-east-1.amazonaws.com/stage";
     public const string identityPoolID = "us-east-1:48a80481-0445-4b59-a477-e67200af0d4e";
     public const string regionString = "us-east-1";
